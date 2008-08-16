eb570b7cf28493335b339a85a069e2ae423106a9
hacintosh <harmonjm@gmail.com>
2008-08-16 18:29:08 -0400

    pretty much finished the round robin schedule creation and also added a to_csv method

--- round_robin.rb
+++ round_robin.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 class Schedule
-	attr_reader :teams, :weeks, :schedule
+	attr_reader :teams, :weeks, :schedule, :schedule_table
 
 	# create a new empty schedule with given teams, and weeks.
 	def initialize(teams, weeks)
@@ -11,45 +11,58 @@ class Schedule
 		@weeks = weeks
 		@schedule = []
 	end
-	# prints the schedule
-	#~ def print(io = $stdout)
-		#~ io.puts "schedule:"
-		#~ w = 0
-		#~ schedule.each do |week|
-			#~ io.printf "%4d: ", w
-			#~ io.puts
-			#~ w += 1
-		#~ end
-	#~ end
+	# prints the schedule in a csv format
+	def to_csv(io = $stdout)
+		io.puts "#Round Robin Tournament Schedule"
+		io.printf "#%s Teams, %s Weeks\n", teams, weeks
+		io.puts "#week, home_team, away_team"
+		
+		schedule.each do |game|
+			io.printf "%s,%s,%s\n", game["week"], game["home_team"], game["away_team"]
+		end
+	end
 end
 
 class RoundRobinSchedule < Schedule
 
 	# generate schedule via cyclic algorithm
-	def generate	
+	def create
 		locked_item = teams - 1
 		weeks.times do |week|
 			week_array = []
-			#~ puts "Week #{week}"
-			week_array << "#{week + 1}:"
 			week_array << locked_item + 1
 
 			(teams - 1).times do |i|
 				week_array << ((week + i) % (teams - 1)) + 1
 			end
-			
-			for x in week_array
-				print x.to_s + " "
-			end
-			puts
-		end
 
+			(1..teams/2).each do |i|
+				even_odd_game = i % 2
+			
+				if i == 1
+					if week % 2 == 0
+						game = {"week" => week + 1, "home_team" => week_array.first, "away_team" => week_array.last}
+					else
+						game = {"week" => week + 1, "home_team" => week_array.last, "away_team" => week_array.first}
+					end
+				else
+					if i % 2 == 0
+						game = {"week" => week + 1, "home_team" => week_array.first, "away_team" => week_array.last}
+					else
+						game = {"week" => week + 1, "home_team" => week_array.last, "away_team" => week_array.first}
+					end
+				end
+				week_array.shift
+				week_array.pop
+				schedule << game
+			end
+		end
 	end
 end
 
 
-puts "Round Robin Generator"
-
+puts "Round Robin Generator Example"
+puts
 schedule = RoundRobinSchedule.new(16,17)
-schedule.generate
-#~ schedule.print
+schedule.create
+schedule.to_csv
